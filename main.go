e25abd4c02a15d889c55e7c54674288161a7ccb7
Vlad Lialin <guitaricet@gmail.com>
2022-11-05 19:30:03 -0400

    add config file deprecate HOWTO_OPENAI_MODEL env variable

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
+	"math/rand"
 	"net/http"
 	"os"
+	"path/filepath"
 	"runtime"
 	"strings"
 	"time"
@@ -21,7 +24,82 @@ type Choice struct {
 	Text string `json:"text"`
 }
 
-const VERSION = "1.0.3"
+type HowtoConfig struct {
+	Model     string `json:"model"`
+	Shell     string `json:"shell"`
+	MaxTokens int    `json:"max_tokens"`
+}
+
+type CachedResponse struct {
+	Question string `json:"question"`
+	Answer   string `json:"answer"`
+}
+
+type HowToState struct {
+	Version     string           `json:"version"`
+	Cache       []CachedResponse `json:"cache"`
+	LastWarning time.Time        `json:"lastWarning"`
+}
+
+const VERSION = "1.1.0-dev"
+const DEFAULT_CONFIG = `{
+	"model": "text-davinci-002",
+	"shell": "bash",
+	"max_tokens": 256
+}`
+
+func getConfigPath() string {
+	if runtime.GOOS == "windows" {
+		return os.Getenv("APPDATA") + "\\howto\\config.json"
+	} else {
+		return os.Getenv("HOME") + "/.howto/config.json"
+	}
+}
+
+func getConfig() (HowtoConfig, error) {
+	var config HowtoConfig
+
+	configPath := getConfigPath()
+	_, err := os.Stat(configPath)
+	if os.IsNotExist(err) {
+		return config, err
+	}
+
+	file, err := os.Open(configPath)
+	if err != nil {
+		return config, err
+	}
+
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&config)
+	if err != nil {
+		return config, err
+	}
+
+	return config, nil
+}
+
+func checkEnv() {
+	hasError := false
+	if os.Getenv("OPENAI_API_KEY") == "" {
+		fmt.Println("Please set the OPENAI_API_KEY environment variable")
+		fmt.Println("You can get an API key from https://beta.openai.com/docs/quickstart/add-your-api-key")
+		fmt.Println("Once you have an API key, set it in your environment with `export OPENAI_API_KEY=<your key>`")
+		hasError = true
+	}
+
+	if os.Getenv("HOWTO_OPENAI_MODEL") != "" {
+		// this is a very anoying message, so let's only print it with 20% probability
+		if rand.Intn(100) < 20 {
+			fmt.Printf("The HOWTO_OPENAI_MODEL environment variable is deprecated. ")
+			fmt.Printf("use the config file %s instead.\n", getConfigPath())
+		}
+	}
+
+	if hasError {
+		os.Exit(1)
+	}
+}
 
 func printEnvInfo() {
 	fmt.Println("Howto version: " + VERSION)
@@ -36,12 +114,74 @@ func printEnvInfo() {
 		fmt.Println("OpenAI API key: invalid (does not start with sk-)")
 	}
 
-	modelName := os.Getenv("HOWTO_OPENAI_MODEL")
-	if modelName == "" {
-		fmt.Println("OpenAI model: not set")
+	config, err := getConfig()
+	if os.IsNotExist(err) {
+		fmt.Println("Config file: not found")
+	} else if err != nil {
+		fmt.Println("Config path: " + getConfigPath())
+		fmt.Println("Error reading config file: " + err.Error())
 	} else {
-		fmt.Println("OpenAI model: " + modelName)
+		fmt.Println("Config path: " + getConfigPath())
+		fmt.Printf("Config: %v", config)
 	}
+}
+
+func setup() {
+	fmt.Println("First time setup")
+	if os.Getenv("OPENAI_API_KEY") == "" {
+		fmt.Println("Please set the OPENAI_API_KEY environment variable")
+		fmt.Println("You can get an API key from https://beta.openai.com/docs/quickstart/add-your-api-key")
+		fmt.Println("Once you have an API key, set it in your environment with `export OPENAI_API_KEY=<your key>`")
+		os.Exit(1)
+	}
+
+	configPath := getConfigPath()
+	fmt.Println("Creating default config at" + configPath)
+
+	configDir := filepath.Dir(configPath)
+	if err := os.MkdirAll(configDir, os.ModePerm); err != nil {
+		fmt.Println("Error creating config directory: " + err.Error())
+		os.Exit(1)
+	}
+
+	file, err := os.Create(configPath)
+	if err != nil {
+		fmt.Println("Error creating config file: " + err.Error())
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	fmt.Println("What shell do you use?")
+	fmt.Println("If you don't know, just press enter to use the default (bash)")
+	fmt.Println("You can change this later in the config file " + configPath)
+	fmt.Println("Options: bash, zsh, fish, powershell")
+
+	var shell string
+	fmt.Scanln(&shell)
+	if shell == "" {
+		shell = "bash"
+	}
+	shell = strings.ToLower(shell)
+	fmt.Println("Setting shell to " + shell)
+
+	var config HowtoConfig
+	err = json.Unmarshal([]byte(DEFAULT_CONFIG), &config)
+	if err != nil {
+		fmt.Println("Error parsing default config: " + err.Error())
+		fmt.Println("This is a serious bug, please report it at https://github.com/guitaricet/howto/issues with the following information:")
+		fmt.Println("Default config string:\n" + DEFAULT_CONFIG)
+		os.Exit(1)
+	}
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ")
+	err = encoder.Encode(config)
+	if err != nil {
+		fmt.Println("Error writing config file: " + err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Printf("Setup complete. Now you can use howto!\n\n")
 }
 
 func main() {
@@ -55,22 +195,27 @@ func main() {
 		return
 	}
 
+	_, err := os.Stat(getConfigPath())
+	if os.IsNotExist(err) {
+		setup()
+	}
+
+	checkEnv() // guarantees that OPENAI_API_KEY is set
+
 	httpkey := os.Getenv("OPENAI_API_KEY")
-	if httpkey == "" {
-		fmt.Println("OPENAI_API_KEY not set")
-		fmt.Println("You can get an API key from https://beta.openai.com/docs/quickstart/add-your-api-key")
-		fmt.Println("Once you have an API key, set it in your environment with `export OPENAI_API_KEY=<your key>`")
+
+	config, err := getConfig()
+	if err == io.EOF {
+		setup()
+	} else if err != nil {
+		fmt.Println("Error reading config file: " + err.Error())
 		os.Exit(1)
 	}
 
-	// get env variable HOWTO_OPENAI_MODEL if it exists, else use code-davinci-002
-	modelName := os.Getenv("HOWTO_OPENAI_MODEL")
-	if modelName == "" {
-		modelName = "text-davinci-002"
-	}
-
 	input := strings.Join(os.Args[1:], " ")
-	prompt := fmt.Sprintf("Bash command to %s:```", input)
+
+	// prompt example: "bash command to tar file without compression: ```[insert]```"
+	prompt := fmt.Sprintf("%s command to %s:```", config.Shell, input)
 	suffix := "```"
 
 	body := []byte(fmt.Sprintf(`{
@@ -78,11 +223,11 @@ func main() {
 		"prompt": "%s",
 		"suffix": "%s",
 		"temperature": 0,
-		"max_tokens": 256,
+		"max_tokens": %d,
 		"top_p": 1,
 		"frequency_penalty": 0,
 		"presence_penalty": 0
-	}`, modelName, prompt, suffix))
+	}`, config.Model, prompt, suffix, config.MaxTokens))
 
 	req, err := http.NewRequest("POST", "https://api.openai.com/v1/completions", bytes.NewBuffer(body))
 	if err != nil {
@@ -111,7 +256,9 @@ func main() {
 
 	choices := openaiResponse.Choices
 	if len(choices) == 0 {
-		fmt.Println("OpenAI API disn't respont correctly. Did you correctly set you OPENAI_API_KEY?")
+		fmt.Println("OpenAI API disn't respont correctly. Did you correctly set OPENAI_API_KEY?")
+		// more info about the response
+		fmt.Println("Response body: ", string(body))
 		fmt.Println("Response: ", resp)
 		os.Exit(1)
 	}
