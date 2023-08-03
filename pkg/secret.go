9f81982bb06a6a50a7deafb82c63abb656f683ab
Vlad Lialin <guitaricet@gmail.com>
2023-08-03 12:43:12 -0400

    hotfix the case when keyring exists on linux

--- pkg/secret.go
+++ pkg/secret.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"runtime"
 
 	"github.com/zalando/go-keyring"
 )
@@ -21,16 +22,16 @@ func SetOpenAiApiKey(apiKey string) error {
 func GetOpenAiApiKey() (string, error) {
 	secret, err := keyring.Get(SERVICE_NAME, "openai_api_key")
 
-	// check if it exists at all
-	if err == keyring.ErrNotFound {
-		fmt.Println("OpenAI API key not found. Please run `howto --setup` to set it.")
-		return "", err
-	}
-
-	if err != nil {
-		// many issues on Linux, so try to get it from the environment
+	if runtime.GOOS == "darwin" {
+		// check if it exists at all
+		if err == keyring.ErrNotFound {
+			fmt.Println("OpenAI API key not found. Please run `howto --setup` to set it in keyring.")
+			return "", err
+		}
+	} else {
+		// many issues with keyring on Linux, use env var
 		secret = os.Getenv("OPENAI_API_KEY")
-		return secret, nil
+		err = nil
 	}
 
 	// check if it's valid
